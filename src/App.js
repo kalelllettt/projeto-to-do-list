b4d03ba52564bc0c24ffa49e74660e2804903fd1
Kalel <kalel@acstecnologiaeservicos.com.br>
2022-12-27 17:50:40 -0300

    estilização

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 function App() {
-  return <div><h1>TO DO LIST</h1></div>;
+  return (
+    <div style={{display:"flex", alignItens:"center", flexDirection:"column", backgroundColor:"rgb(198, 86, 0)"}}>
+      <h1 style={{display:"flex", alignItems:"center", flexDirection:"column", color: "blue", margin:"84 0 24",  }}>TO DO LIST</h1>
+      <input style={{backgroundColor:"rgb(156, 146, 225)", padding:"50"}} type="text" placeholder="Digite Aqui" />
+      <button type="button" style={{backgroundColor:"rgb(58, 25, 79)", color:"rgb(12, 125, 125)"}}>Adicionar</button>
+    </div>
+  );
 }
 
 export default App;
